4c2dad9f7f7b452a09d25c1fa213b54f1506f2ab
Jerome Amiguet <jerome.amiguet@epfl.ch>
2023-03-21 01:34:50 +0100

    Implement all in one file for now

--- thonnycontrib/thonny-memorycode/__init__.py
+++ thonnycontrib/thonny-memorycode/__init__.py
@@ -1,19 +1,73 @@
 from thonny import get_workbench
 from tkinter.messagebox import showinfo, showerror
-from memorycode import Memorycode
+from git import Repo, exc
+#from memorycode import Memorycode
 import os
 
 MODULE_NAME = "memorycode"
+class Memorycode:
+    def __init__(self, output=lambda x:x):
+        self.output = output
+        
+    def set_directory(self, path=None):
+        try:
+            if path == None:
+                self.repo = Repo(search_parent_directories=True)
+            else:
+                self.repo = Repo(path)
+                
+        except exc.InvalidGitRepositoryError:
+            self.repo = None
+            self.output("No repository set.")
+            
+    def save(self, message="commit from Thonny"):
+        self.__commit(message)
+        self.__push_with_ssh(".ssh/id_ed25519")
+      #  self.__push_with_http("ja-edu", "Ejp45314531$")
+    
+    def list_save(self):
+        if self.repo is not None:
+            commits = list(self.repo.iter_commits())
+            for commit in commits:
+                self.output(commit.hexsha[-8:] + "  " + commit.message)
 
-def commit():
-    try:
-        repo = git.Repo(search_parent_directories=True)
-        repo.git.add('--all')
-        repo.index.commit("commit from Thonny")
-        showinfo("MODULE_NAME", "Code committed successfully.")
-    except Exception as e:
-        showerror("Failed to commit code", str(e))
+    
+    def git_diagnostic(self):
+        if self.repo is not None:
+            try:
+                self.output("Repo:", self.repo)
+                self.output("Remote:", self.repo.remotes)
+            except Exception as e:
+                self.output("No git" + str(e))
+    
+    def __commit(self, commit_message):
+        if self.repo is not None:
+            try:
+                self.repo.git.add('--all')
+                self.repo.index.commit(commit_message)
+                self.output("Code committed successfully.")
+            except Exception as e:
+                self.output("Failed to commit code " + str(e))
 
+    def __push_with_ssh(self,ssh_key_path):
+        if self.repo is not None and self.repo.remotes:
+            # Important not to check for known_hosts
+            ssh_command = f"ssh -v -i {ssh_key_path} -o StrictHostKeyChecking=no"
+            #  os.system("ssh-agent bash -c 'ssh-add .ssh/id_ed25519 ; git push '")
+            with self.repo.git.custom_environment(GIT_SSH_COMMAND=ssh_command):
+                self.repo.remote().push(refspec=self.repo.head.reference)
+                self.output("Pushed successfully.")
+                
+    def create_and_checkout_branch(self, branch_name):
+        if self.repo is not None:
+            # Vérifier si la branche existe déjà
+            if branch_name in self.repo.branches:
+                # La branche existe, checkout
+                self.repo.git.checkout(branch_name)
+            else:
+                # La branche n'existe pas, la créer et checkout
+                self.repo.git.branch(branch_name)
+                self.repo.git.checkout(branch_name)
 
 def info():
     current_tab = get_workbench().get_editor_notebook().get_current_editor()
@@ -34,9 +88,6 @@ def get_current_file_directory():
             return directory
 
 
-def switch_tab():
-    showinfo(MODULE_NAME, "New tab")
-
 def before_running():
     showinfo(MODULE_NAME, "before_running")
     # commit_code()
@@ -44,21 +95,22 @@ def before_running():
 
 def load_plugin():
     #init_module()
+    memorycode = Memorycode(output=lambda x : showinfo(MODULE_NAME, x))
     workbench = get_workbench()
     workbench.add_command(command_id="info",
                           menu_name="tools",
                           command_label="info",
                           handler=info)
-    workbench.add_command(command_id="git",
+    workbench.add_command(command_id="save",
                           menu_name="tools",
-                          command_label="git",
-                          handler=commit)
+                          command_label="save",
+                          handler=memorycode.save)
 
-    workbench.bind("WorkbenchClose", before_running)
-    workbench.bind("NewFile", lambda arg: showinfo("new", arg))
-    workbench.bind("RunFile", lambda arg: showinfo("run1", arg))
-    workbench.bind("Save", lambda arg: showinfo("run2", arg))
-    workbench.bind("RemoteFilesChanged", lambda arg: showinfo("run3", arg))
-    workbench.bind("<<NotebookTabChanged>>", lambda arg: showinfo("run4", arg))
+   # workbench.bind("WorkbenchClose", before_running)
+   # workbench.bind("NewFile", lambda arg: showinfo("new", arg))
+   # workbench.bind("RunFile", lambda arg: showinfo("run1", arg))
+    workbench.bind("Save", lambda x: memorycode.save())
+   # workbench.bind("RemoteFilesChanged", lambda arg: showinfo("run3", arg))
+    workbench.bind("<<NotebookTabChanged>>", lambda x : memorycode.set_directory(path=get_current_file_directory()))
     # workbench.bind("<<TextChange>>", lambda arg: showinfo("run4", arg))
 
