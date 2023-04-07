7f978a345506ca47de355ac0f631c7035e9f9b1a
unknown <jerome.amiguet@epfl.ch>
2023-04-07 12:46:49 +0200

    Implement git save in memorycodeView

--- thonnycontrib/memorycode/memorycodeView.py
+++ thonnycontrib/memorycode/memorycodeView.py
@@ -1,4 +1,5 @@
-from tkinter import ttk, Canvas, Frame, Label, Scrollbar
+from tkinter import ttk, Canvas, Frame, Label, Scrollbar, Text
+from time import strftime, gmtime
 
 class MemorycodeView (ttk.Frame):
     def __init__(self, parent, *args, **kwargs):
@@ -8,29 +9,41 @@ class MemorycodeView (ttk.Frame):
         self.columnconfigure(1, weight=1)
         self.rowconfigure(1, weight=1)
 
-        self._label = ttk.Label(self, text="Memorycode")
-        self._label.grid(row=0, column=0, sticky="nsew")
+        self.top_frame = ttk.Frame(self)
+        self.top_frame.grid(row=0, column=0, sticky="nsew")
+        label = ttk.Label(self.top_frame, text="Memorycode")
+        label.grid(row=0, column=0, sticky="nsew")
+        combobox = ttk.Combobox(self.top_frame)
+        combobox.grid(row=0, column=1, sticky="nsew")
 
-        canvas = Canvas(self, bg="purple")
+        canvas = Canvas(self, bg="white")
 
         scrollbar = Scrollbar(self, orient="vertical", command=canvas.yview)
         self.scrollable_frame = Frame(canvas, bg="white")
 
-        self.scrollable_frame.bind(
-            "<Configure>",
-            lambda e: canvas.configure(
-                scrollregion=canvas.bbox("all")
-            )
-        )
+     #   self.scrollable_frame.bind(
+      #      "<Configure>",
+       #     lambda e: canvas.configure(
+        #        scrollregion=canvas.bbox("all")
+         #   )
+        #)
 
 #        self.scrollable_frame.update_idletasks()  # Update frame info before adding it to canvas
         canvas.create_window((0, 0), anchor="nw", window=self.scrollable_frame)
-       # canvas.configure(scrollregion=canvas.bbox("all"))
+     #   canvas.configure(scrollregion=canvas.bbox("all"))
+        canvas.bind('<Configure>', lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
         canvas.configure(yscrollcommand=scrollbar.set)
         canvas.grid(row=1, column=0, sticky="nsew")
         scrollbar.grid(row=1, column=0, sticky="nse")
 
         #self.from_saves([i for i in range(50)])
+
+    def set_projects_list(self, projects, active=None, callback=None):
+        self.top_frame.children["!combobox"].config(values=projects)
+        if active is not None:
+            self.top_frame.children["!combobox"].current(active)
+        if callback is not None:
+            self.top_frame.children["!combobox"].bind("<<ComboboxSelected>>", callback)
 
     def from_saves(self, saves):
         if saves is None:
@@ -40,17 +53,43 @@ class MemorycodeView (ttk.Frame):
         for i in range(len(saves)):
             rect = Canvas(self.scrollable_frame, bg="blue", height=50)
             rect.grid(row=i, column=0, sticky="nsew")
-            label = Label(rect, text=f"{i} " + saves[i].summary, fg="white", bg="blue")
-            label.place(relx=0.5, rely=0.5, anchor="center")
+            rect.bind("<1>", lambda x : print(x))
+            text = Text(rect, fg="white", bg="blue", height=rect.winfo_height()*3)
+            text.insert("0.0", saves[i].summary + "\n" + saves[i].committer.name + "\n" + strftime("%a, %d %b %Y %H:%M", gmtime(saves[i].committed_date)))
+            text.config(state=('disabled' if i != 0 else 'normal'), cursor=('arrow' if i != 0 else 'xterm'))
+            text.save = saves[i]
+            #text.bindtags(tuple(list(text.bindtags()).insert(1, rect)))
+            if i != 0:
+                text.bind("<1>", lambda x : print(str(x.widget.save.hexsha) + "  " + str(x) + '\n' + '\n'.join(map(str, [x.num, x.widget, x.widget.get(2.0, '2.end')]))))
+            text.grid(row=0, column=0, sticky="nw")
+            #label.place(relx=0.5, rely=0.5, anchor="center")
+
+            rect.columnconfigure(1, weight=1)
+            rect.rowconfigure(1, weight=1)
 
 if __name__ == "__main__":
     # For testing
     from tkinter import Tk
+    from time import time
     root = Tk()
     root.geometry("400x400")
     root.columnconfigure(0, weight=1)
     root.rowconfigure(0, weight=1)
 
-    MemorycodeView(root).grid(row=0, column=0, sticky="nsew")
+    memorycode = MemorycodeView(root)
+    memorycode.grid(row=0, column=0, sticky="nsew")
+    class FakeAuthor:
+        def __init__(self, name):
+            self.name = name
+    class FakeCommit:
+        def __init__(self, message, hexsha="1234567890abcdef1234567890abcdef12345678"):
+            self.message = message
+            self.committed_date = time()
+            self.committer = FakeAuthor("Harold")
+            self.summary = message
+            self.hexsha = hexsha
+    memorycode.from_saves([FakeCommit(f"summary {i}", i) for i in range(50)])
+    projects = ["Project 1", "Project 2", "Project 3"]
+    memorycode.set_projects_list(projects, projects.index("Project 2"))
 
     root.mainloop()
